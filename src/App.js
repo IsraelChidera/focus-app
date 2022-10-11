c1604e2c8a29c8c95b1d1c0b18924d12e6bf4e78
IsraelChidera <israelchidera@gmail.com>
2022-10-11 12:10:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
